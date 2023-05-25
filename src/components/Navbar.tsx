00fbe067b0d04f9f4d9aa8287a5fdafa1d37c98a
bigwhitewolfgang <codingwhitewolf@proton.me>
2023-05-26 03:37:49 +0530

    fix: minor issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
     return (
-        <div style={{ background: "#EF4D90", height: "8.5rem", display: "flex", justifyContent: 'center'}}>
-            <h1 style={{ fontSize: "5.5rem" }}>$SPANK</h1>
+        <div style={{ background: "#EF4D90", height: "8.5rem", display: "flex", justifyContent: 'center' }}>
+            <h1 style={{ fontSize: "5.5rem", marginTop: "0.78rem" }}>$SPANK</h1>
         </div>
     )
 }
